9d42e742395e794d5319bf086d7f049a9132917c
shektor <s.hektor@gmail.com>
2019-07-25 16:45:28 +0100

    Create standup program
    A command line program to facilitate a solo stand-up when there is no 
    one else to do a stand-up with.

---
+++ 01_standup/program.py
@@ -0,0 +1,18 @@
+print('STANDUP APP')
+
+user_text = input('What is your name? ')
+greeting = 'Hey ' + user_text + '!'
+
+print(greeting)
+
+feeling_text = input('How are you feeling today? ')
+feeling = 'Thank you for sharing.'
+
+print(feeling)
+
+accountable_text = input('What should we hold you accountable for today? ')
+
+summary = 'You are feeling ' + feeling_text + ' today, and want to ' + accountable_text
+
+print(summary)
+print('Lets do it!')
