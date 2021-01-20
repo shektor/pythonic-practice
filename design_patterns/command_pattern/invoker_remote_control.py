26c60fa6f20fa00282ab570f73d5019d6f21c9a4
shektor <s.hektor@gmail.com>
2021-01-20 21:44:06 +0000

    Enable undo of commands

--- design_patterns/command_pattern/invoker_remote_control.py
+++ design_patterns/command_pattern/invoker_remote_control.py
@@ -1,12 +1,20 @@
+from queue import LifoQueue
 
 class RemoteControl():
 
     def __init__(self, on, off):
+        self._undo_commands = LifoQueue()
         self._on = on
         self._off = off
     
     def click_on(self):
         self._on.execute()
+        self._undo_commands.put(self._on)
     
     def click_off(self):
         self._off.execute()
+        self._undo_commands.put(self._off)
+
+    def undo_command(self):
+        if not self._undo_commands.empty():
+            self._undo_commands.get().undo()
